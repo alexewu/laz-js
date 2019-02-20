d3ef369466612ba7ddc849735395452c8d4b8ec8
alexewu <alexewu@umich.edu>
2019-02-20 18:44:27 -0500

    Pluralsight tutorial product that works with github api

---
+++ angular.js
@@ -0,0 +1,52 @@
+//var app = angular.module('myApp', []);
+//
+//app.controller('MainController', function($scope, $http) {
+//    var onUserComplete = function(response) {
+//        $scope.user = response.data;
+//    };
+//
+//    var onError = function(reason) {
+//        $scope.error = "Could not read";
+//    };
+//    
+//    $http.get("https://api.github.com/users/alexewu")
+//        .then(onUserComplete, onError)
+//    
+//    
+//    $scope.message = "Working with Github API";
+//});
+
+
+
+(function() {
+    var app = angular.module('myApp', []);
+    
+    var MainController = function ($scope, $http) {
+            
+        var onUserComplete = function(response) {
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+        
+        var onRepos = function(response) {
+            $scope.repos = response.data;
+        }
+        
+        var onError = function(reason) {
+            $scope.error = "Could not read";
+        };
+        
+        $scope.search = function(username) {
+            $http.get("https://api.github.com/users/" + username)
+            .then(onUserComplete, onError)
+        }
+
+        $scope.username = "HELLO MY LAZ FRIENDS";
+        $scope.message = "Working with Github API";
+        $scope.repoSortOrder = "-stargazers_count";
+    };
+    
+    app.controller("MainController", MainController)
+    
+}());
