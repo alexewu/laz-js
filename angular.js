d15abbba9d70b4d849213e1de26cd98c90f7fd40
alexewu <alexewu@umich.edu>
2019-03-14 16:38:52 -0400

    commit before working on

--- angular.js
+++ angular.js
@@ -1,23 +1,3 @@
-//var app = angular.module('myApp', []);
-//
-//app.controller('MainController', function($scope, $http) {
-//    var onUserComplete = function(response) {
-//        $scope.user = response.data;
-//    };
-//
-//    var onError = function(reason) {
-//        $scope.error = "Could not read";
-//    };
-//    
-//    $http.get("https://api.github.com/users/alexewu")
-//        .then(onUserComplete, onError)
-//    
-//    
-//    $scope.message = "Working with Github API";
-//});
-
-
-
 (function() {
     var app = angular.module('myApp', []);
     
@@ -36,28 +16,14 @@
         var onError = function(reason) {
             $scope.error = "Could not read";
         };
-        
-        // $scope.search = function(teacher_name, kid_name) {
-        //     $http({
-        //         method: 'GET',
-        //         url: 'http://mdp.learninga-z.com/api/students'
-        //         //params: {teacher: teacher_name, kid: kid_name}
-        //     }).then(onUserComplete, onError);
-        // }
-        
 
         var mainInfo = $http({method: 'GET', url: 'http://mdp.learninga-z.com/api/students'})
             .then(function(data) { 
                 console.log(json.loads(data));
             });
 
-//        $scope.search = function(teacher_name, kid_name) {
-//            $http(.get("api/{bookid}/timestamp")
-//            .then(onUserComplete, onError))
-//        }
-
         $scope.username = "HELLO MY LAZ FRIENDS";
-        $scope.message = "Working with Github API";
+        $scope.message = "Teacher Front End";
         $scope.repoSortOrder = "-stargazers_count";
     };
     
