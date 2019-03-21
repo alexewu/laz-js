e03ed7df2f22a4f769a4842f141fa595bf2f4fa0
alexewu <alexewu@umich.edu>
2019-03-21 19:37:55 -0400

    got rid of student search

--- angular.js
+++ angular.js
@@ -5,22 +5,27 @@
             
         var onUserComplete = function(response) {
             $scope.user = response.data;
+            //TO-DO: change $scope.user.repos_url to correct field of laz api
             $http.get($scope.user.repos_url)
-                .then(onRepos, onError);
+                .then(onWords, onError);
         };
         
-        var onRepos = function(response) {
-            $scope.repos = response.data;
+        //funtion that fetches each word in the high frequency list
+        var onWords = function(response) {
+            $scope.words = response.data;
         }
         
         var onError = function(reason) {
             $scope.error = "Could not read";
         };
-
-        var mainInfo = $http({method: 'GET', url: 'http://mdp.learninga-z.com/api/students'})
+        
+        //now can use the student variable in the api get call
+        $scope.search = function(student){
+            var mainInfo = $http({method: 'GET', url: 'http://mdp.learninga-z.com/api/students'})
             .then(function(data) { 
                 console.log(json.loads(data));
             });
+        }
 
         $scope.username = "HELLO MY LAZ FRIENDS";
         $scope.message = "Teacher Front End";
